6ab5d83942c37cd5f7707826d5f4d95a50afd9b7
Scott Moon <interactord@gmail.com>
2019-05-08 05:48:39 +0900

    feat 버튼 빌더 추가
    UIButton을 빌더 형태로 만들어주는 기능이 추가 되었습니다.

---
+++ SCUIBuildKit/Sources/Implementation/ButtonBuilder.swift
@@ -0,0 +1,30 @@
+//
+// Created by Scott Moon on 2019-05-07.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import UIKit
+
+public struct ButtonBuilder: Builder, CALayerUsable, BackgroundColorable {
+  public var targetView: UIButton
+
+	public init(type: UIButton.ButtonType? = nil) {
+    targetView = UIButton(type: type ?? .system)
+	}
+
+  public func setTitle(_ title: String, state: UIControl.State? = nil) -> ButtonBuilder {
+		targetView.setTitle(title, for: state ?? .normal)
+		return self
+  }
+
+  public func setFont(_ font: UIFont) -> ButtonBuilder {
+    targetView.titleLabel?.font = font
+		return self
+  }
+
+	public func setTitleColor(_ color: UIColor, state: UIControl.State? = nil) -> ButtonBuilder {
+		targetView.setTitleColor(color, for: state ?? .normal)
+		return self
+	}
+
+}
---
+++ SCUIBuildKit/Sources/Implementation/ButtonBuilderSpec.swift
@@ -0,0 +1,50 @@
+//
+// Created by Scott Moon on 2019-05-07.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import XCTest
+import UIKit
+
+@testable import SCUIBuildKit
+
+class ButtonBuilderSpec: XCTestCase {
+	var sut: UIButton?
+
+	override func setUp() {
+		super.setUp()
+	}
+
+	override func tearDown() {
+		super.tearDown()
+		sut = nil
+	}
+
+	func test_setTitle() {
+		let expectedTitle = "Test"
+
+		sut = ButtonBuilder()
+			.setTitle("Test")
+			.build()
+
+		XCTAssertEqual(expectedTitle, sut?.titleLabel?.text)
+	}
+
+	func test_setFont() {
+
+		let expectedFont = UIFont.systemFont(ofSize: 20)
+		sut = ButtonBuilder()
+			.setFont(expectedFont)
+			.build()
+
+		XCTAssertEqual(expectedFont, sut?.titleLabel?.font)
+	}
+
+	func test_setTitleColor() {
+		let expectedColor = UIColor.red
+
+		sut = ButtonBuilder()
+			.setTitleColor(expectedColor)
+			.build()
+	}
+}
